51d2ea2dce403deb2f8706fa9efd40f593b3d68f
Phil Tooley <phil.tooley@sheffield.ac.uk>
2019-02-05 20:39:28 +0000

    hotfix for out of bounds access bug in image loader

--- src/map.cpp
+++ src/map.cpp
@@ -296,8 +296,8 @@ Vec_unique Map::get_dim_data_dmda_blocked(uinteger dim) const
 void Map::calculate_basis()
 {
   // Get the full Nd basis
-  floatvector scalings(m_ndim, 0.0);
-  floatvector offsets(m_ndim, 0.0);
+  floatvector scalings(this->m_v_node_spacing.size(), 0.0);
+  floatvector offsets(this->m_v_node_spacing.size(), 0.0);
 
   std::transform(this->m_v_node_spacing.begin(), this->m_v_node_spacing.end(), scalings.begin(),
       [](floating a) -> floating { return 1 / a; });
