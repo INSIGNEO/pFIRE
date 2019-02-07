15242f32841a89893a9fe090ff3ed2def0dc19e4
Phil Tooley <phil.tooley@sheffield.ac.uk>
2019-02-07 17:45:27 +0000

    minor formatting fix

--- upload_docs.sh
+++ upload_docs.sh
@@ -14,7 +14,7 @@ zip -r _static/pfire_tutorial_files.zip tutorial_files
 export PATH="${PATH}:${basedir}/bin"
 sphinx-build -an $docdir $ghpdir
 cd $ghpdir
-#git add --all
-#git commit -m "$(date)"
-#git push upstream gh-pages
+git add --all
+git commit -m "$(date)"
+git push upstream gh-pages
 )
